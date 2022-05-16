4d4d02e4132b58587dfa6aefdc6d0c09bfd7e26d
Vyshnav <Vyshnavc235@gmail.com>
2022-05-16 20:41:55 +0530

    admin login page done

--- admin/mainpage.php
+++ admin/mainpage.php
@@ -4,20 +4,21 @@
 <link rel="stylesheet" type="text/css" href="login1.css">
 <link rel="stylesheet" type="text/css" href="css/header.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 <head>
-	
-	
+
+
 	<title>
-	Pharma  | Admin Login
+		Pharma | Admin Login
 	</title>
 </head>
 
 <body>
-<?php include('includes/header.php');?>
-	<br><br><br><br>
-	<div class="container">
-		<form method="post" action="">
-			<div style="width: 50%;" id="div_login">
+	<?php include('includes/header.php'); ?>
+
+	<div class="container mt-3">
+		<form class="text-center " method="post" action="">
+			<div class="mt-5" style="width: 40%;" id="div_login">
 				<h1 class="text-center">Admin Login</h1>
 				<center>
 					<div>
@@ -28,7 +29,7 @@
 					</div>
 					<div>
 
-						<input type="submit" value="Submit" name="submit" id="submit" />
+						<input type="submit" value="SUBMIT" name="submit" id="submit" />
 						<input type="submit" value="Click here for Pharmacist Login" name="psubmit" id="submit" />
 				</center>
 				<?php
@@ -65,11 +66,11 @@
 	</div>
 	</form>
 	</div>
-	<?php include('includes/footer.php');?>
-	
+	<?php include('includes/footer.php'); ?>
+
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>	
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
