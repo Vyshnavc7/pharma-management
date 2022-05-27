e714dec9936740f42f3a24d257f0b8e1a9d53cca
Vyshnav <Vyshnavc235@gmail.com>
2022-05-27 18:30:47 +0530

    admin pages header and sidebar done

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,117 @@
+<nav style="background-color: #3b3a3a;" class="sidebar sidebar-offcanvas" id="sidebar">
+    <div class="user-profile">
+        <div class="user-image">
+            <img src="images/faces/pharm1.png">
+        </div>
+        <div class="user-name">
+            Pharma Management
+        </div>
+        <div class="user-designation">
+            Admin
+        </div>
+    </div>
+    <ul class="nav">
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" href="index.php">
+                <i class="icon-box menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="icon-disc menu-icon"></i>
+                <span class="menu-title">Inventory</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="inventory-add.php">Add New Medicine</a></li>
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="inventory-view.php">Manage Inventory</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="icon-disc menu-icon"></i>
+                <span class="menu-title">Suppliers</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="supplier-add.php">Add New Supplier</a></li>
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="supplier-view.php">Manage Suppliers</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="icon-disc menu-icon"></i>
+                <span class="menu-title">Stock Purchase</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="purchase-add.php">Add New Purchase</a></li>
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="purchase-view.php">Manage Purchase</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="icon-disc menu-icon"></i>
+                <span class="menu-title">Employees</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="employee-add.php">Add New Employees</a></li>
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="employee-view.php">Manage Employees</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i class="icon-disc menu-icon"></i>
+                <span class="menu-title">Customers</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="customer-add.php">Add New Customers</a></li>
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="customer-view.php">Manage Customers</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" href="sales-view.php">
+                <i class="icon-file menu-icon"></i>
+                <span class="menu-title">Sales Invoice Details</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" href="salesitems-view.php">
+                <i class="icon-pie-graph menu-icon"></i>
+                <span class="menu-title">Sold Products Details</span>
+            </a>
+        </li>
+        
+
+        <li class="nav-item">
+            <a style="border-top: solid;" class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                <i class="icon-head menu-icon"></i>
+                <span class="menu-title">Reports</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="stockreport.php">Medicine:Low stock </a></li>
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="expiryreport.php"> Medicine Expiry </a></li>
+                    <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="salesreport.php"> Transaction Report </a></li>
+
+                </ul>
+            </div>
+
+        </li>
+    </ul>
+</nav>
--- admin/includes/mainheader.php
+++ admin/includes/mainheader.php
@@ -1,8 +1,41 @@
-<div class="header">
-    <div class="header">
-        <h1 style="color: white;" class="text-center">Pharma Management </h1>
-        <p style="color: white;" class="text-center">Mangalore, 575004</p>
-        
+<nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+    <div style="background-color: #3b3a3a;" class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+        <a style="color:  white;" class="navbar-brand brand-logo" href="#">PHARMA</a>
+        <a class="navbar-brand brand-logo-mini" href="#"><img src="images/logo-mini.svg" alt="logo" /></a>
     </div>
-    <a class="text-right" href="logout.php">Logout(Logged in as Admin)</a>
-</div>
+    <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+        <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+            <span class="icon-menu"></span>
+        </button>
+        <strong>
+            <a href="index.php">
+                <p class="mb-0 font-weight-normal float-left dropdown-header"> ADMIN DASHBOARD</p>
+            </a>
+        </strong>
+
+
+        <ul class="navbar-nav navbar-nav-right">
+
+
+            <li class="nav-item dropdown d-flex mr-4 ">
+                <a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center" id="notificationDropdown" href="#" data-toggle="dropdown">
+                    <i class="icon-cog"></i>
+
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+                    <p class="mb-0 font-weight-normal float-left dropdown-header">Settings</p>
+                    <a class="dropdown-item preview-item">
+                        <i class="icon-head"></i> Profile
+                    </a>
+                    <a href="logout.php" class="dropdown-item preview-item">
+                        <i class="icon-inbox"></i> Logout
+                    </a>
+                </div>
+            </li>
+
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+            <span class="icon-menu"></span>
+        </button>
+    </div>
+</nav>
