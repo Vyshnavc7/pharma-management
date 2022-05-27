b5db0a49983a0cc5aff59ef068c28e7532e27ced
Vyshnav <Vyshnavc235@gmail.com>
2022-05-27 17:05:01 +0530

    employee page done

--- employe/includes/leftbar.php
+++ employe/includes/leftbar.php
@@ -52,10 +52,8 @@
             </a>
             <div class="collapse" id="auth">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="../../employe/employe/customer-add.php">ADD NEW </a></li>
-                <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="../../employe/employe/customer-view.php"> VIEW THEM </a></li>
-                
-
+                <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="customer-add.php">ADD NEW </a></li>
+                <li class="nav-item"> <a style="border-top: inset;" class="nav-link" href="customer-view.php"> VIEW THEM </a></li>
               </ul>
             </div>
 
